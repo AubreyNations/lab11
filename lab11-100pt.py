c99ebd006ef20ea389ae571c3ae0a40bd2d73de2
AubreyNations <aubreynations@yahoo.com>
2014-10-31 14:53:14 -0500

    100pt done

--- lab11-100pt.py
+++ lab11-100pt.py
@@ -11,21 +11,21 @@
 from Tkinter import *
 root = Tk()
 # Create the canvas widget
-drawpad = Canvas(root, width=800,height=600, background='white')
+drawpad = Canvas(root, width=800,height=600, background='#7CE7F7')
 drawpad.grid(row=0, column=0)
 
 # Create the oval
-circle = drawpad.create_oval(10, 10, 50, 50, fill='green')
+circle = drawpad.create_oval(10, 10, 100, 100, fill='purple')
 direction = 1
 # Create our animation function
 def animate():
     global direction
     # Get the x and y co-ordinates of the circle
     x1, y1, x2, y2 = drawpad.coords(circle)
-    if x2 > drawpad.winfo_width(): 
-        direction = - 1
-    elif x1 < 0:
-        direction = 1
+    if x2 > drawpad.winfo_width():
+        direction = - 810
+    elif x1 < -5:
+        direction = 5
     #Move our oval object by the value of direction
     drawpad.move(circle,direction,0)
     # Wait for 1 millisecond, then recursively call our animate function
